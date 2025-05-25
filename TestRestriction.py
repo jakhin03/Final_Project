b4c955b2b3b3ff9e245035041e7a765374b40014
thanh2k4 <nvthanh12102004@gmail.com>
2025-05-25 16:46:09 +0700

    fix : fix the test restriction for changes in RestrictionForTimeFrameController

--- TestRestriction.py
+++ TestRestriction.py
@@ -24,7 +24,6 @@ def read_dimac_file(file_path = "TSG.txt"):
                 U = int(parts[4])
                 C = int(parts[5])
                 TSG.append((ID1, ID2,L, U, C))
-    print(virtual_node)
     return TSG
 result = 4
 
@@ -33,21 +32,14 @@ graph_processor = GraphProcessor()
 with patch('builtins.input' , side_effect=["QuardNodes.txt","10" ,"0" , "1","1","1","2 5", "4 1 1 2", "1", "1","","2"]):
     graph_processor.use_in_main()
 
-print(graph_processor.num_max_agvs)
-print(graph_processor.target_nodes)
-print(graph_processor.started_nodes)
 
- 
 restriction = graph_processor.restriction_for_timeframe_controller
-TSG = read_dimac_file("TSG.txt")   
+TSG = read_dimac_file("TSG.txt")
 
-omega = restriction.identify_restricted_edges( [[4,1],[1,2]] , 2, 5 )
-print(omega) 
-restriction_nodes = restriction.indentify_restricted_nodes( omega )
+omega = restriction.get_omega()[0]
+restriction_nodes = restriction.identify_restricted_nodes( omega )
 incoming_capacity_for_restricted_nodes = restriction.calculate_incoming_capacity_for_restricted_nodes( TSG, restriction_nodes)
-print(incoming_capacity_for_restricted_nodes)
 outgoing_capacity_for_restricted_nodes = restriction.calculate_outgoing_capacity_for_restricted_nodes( TSG , restriction_nodes)
-print(outgoing_capacity_for_restricted_nodes)
 max_flow = restriction.calulate_max_flow( omega , incoming_capacity_for_restricted_nodes , outgoing_capacity_for_restricted_nodes )
 assert max_flow == result, f"Max flow should be {result}, but got {max_flow}"
 print(f"Test passed! Max flow is {max_flow}")
